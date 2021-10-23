6c45574d44a3e9d8593a81c1ddc99d12792c5d89
YK@Spectre <eimiveritas@gmail.com>
2021-10-23 21:21:46 +0800

    Able to save order of images

--- lib/edit_project.dart
+++ lib/edit_project.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:ee3080_dip049/export_page.dart';
@@ -31,6 +32,94 @@ List<Map> data = List.generate(
           "name": "Page ${index + 1}",
           "picture": "https://picsum.photos/250?image=$index"
         }).toList();
+
+class PictureObj extends StatelessWidget {
+  PictureObj(
+      {Key? key,
+      required this.picIndex,
+      required this.filePath,
+      required this.removePage,
+      required this.swapPage})
+      : super(key: key);
+
+  int picIndex;
+  String filePath;
+  final Function(int) removePage;
+  final Function(int, int) swapPage;
+
+  @override
+  Widget build(BuildContext context) {
+    var clip = ClipRect(
+      child: Align(
+        alignment: Alignment.topCenter,
+        heightFactor: 1,
+        child: Image.file(File(filePath)),
+      ),
+    );
+    var stack = new Stack(
+      children: <Widget>[
+        Material(
+            color: Colors.amber,
+            child: InkWell(
+              onTap: () {
+                debugPrint("You clicked on page!");
+              },
+              child: clip,
+            )),
+        Positioned(
+          top: 0,
+          right: 0,
+          child: TextButton(
+            onPressed: () {
+              debugPrint("You removed page $this.picIndex");
+              removePage(this.picIndex);
+            },
+            child: Text("X"),
+            style: TextButton.styleFrom(
+                padding: EdgeInsets.all(0),
+                backgroundColor: Colors.blue,
+                primary: Colors.white),
+          ),
+        ),
+        Positioned(
+            bottom: 0,
+            //width: 10,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Page ${this.picIndex} (${filePath.split("/").last})",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    backgroundColor: Colors.blue, color: Colors.white),
+              ),
+            )),
+      ],
+    );
+    var drag = Draggable<String>(
+      data: "${picIndex.toString()}",
+      child: stack,
+      feedback: Flexible(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 150),
+          child: clip,
+        ),
+      ),
+      childWhenDragging: stack,
+    );
+    var droptarget = DragTarget<String>(
+      builder: (BuildContext context, List<String?> incoming, List rejected) {
+        return drag;
+      },
+      onWillAccept: (data) => true,
+      onAccept: (data) {
+        print("Transferring from ${data} to ${picIndex.toString()}");
+        swapPage(int.parse(data), picIndex);
+      },
+      onLeave: (data) {},
+    );
+    return droptarget;
+  }
+}
 
 class _EditProjectPageState extends State<EditProjectPage> {
   List<Widget> listArray = [];
@@ -119,6 +208,7 @@ class _EditProjectPageState extends State<EditProjectPage> {
       print(entity.path);
       listOfDir.add(entity.path);
     }
+    listOfDir.sort();
     return listOfDir;
   }
 
@@ -126,53 +216,54 @@ class _EditProjectPageState extends State<EditProjectPage> {
     // <<<<< Note this change for the return type
     populate(folderPath).then((value) {
       List<Widget> listings = [];
-      for (var i = 0; i < value.length; i++) {
-        print("Done");
-        listings.add(new Stack(
-          children: <Widget>[
-            Material(
-                color: Colors.amber,
-                child: InkWell(
-                  onTap: () {
-                    debugPrint("You clicked on page!");
-                  },
-                  child: ClipRect(
-                    child: Align(
-                      alignment: Alignment.topCenter,
-                      heightFactor: 1,
-                      child: Image.file(File(value[i])),
-                    ),
-                  ),
-                )),
-            Positioned(
-              top: 0,
-              right: 0,
-              child: TextButton(
-                onPressed: () {
-                  debugPrint("You removed page}");
-                  setState(() {});
-                },
-                child: Text("X"),
-                style: TextButton.styleFrom(
-                    padding: EdgeInsets.all(0),
-                    backgroundColor: Colors.blue,
-                    primary: Colors.white),
-              ),
-            ),
-            Positioned(
-                bottom: 0,
-                //width: 10,
-                child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    value[i].split("/").last,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        backgroundColor: Colors.blue, color: Colors.white),
-                  ),
-                )),
-          ],
-        ));
+      File jsonFile = File(arguments["folderPath"] + "/config.json");
+      Map<String, dynamic> jsonFileContent =
+          json.decode(jsonFile.readAsStringSync());
+      List<String> picture_order = [];
+      if (jsonFileContent.containsKey("picture_order")) {
+        // the order was alr there
+        picture_order = jsonFileContent["picture_order"].cast<String>();
+      } else {
+        // new order established
+        // print(jsonFileContent);
+        for (var i = 0; i < value.length; i++) {
+          print("Done");
+
+          String filename = value[i].split("/").last;
+          List<String> reservedFiles = ["config.json"];
+
+          if (!reservedFiles.contains(filename)) {
+            picture_order.add(value[i]);
+          }
+        }
+      }
+
+      for (var i = 0; i < picture_order.length; i++) {
+        PictureObj pic = new PictureObj(
+            picIndex: i,
+            filePath: picture_order[i],
+            removePage: (pageIndex) {
+              setState(() {
+                listArray.removeAt(pageIndex);
+              });
+            },
+            swapPage: (source, dest) {
+              var tempObj = listArray[source];
+              print("S ${(listArray[source] as PictureObj).picIndex}");
+              (listArray[source] as PictureObj).picIndex = dest;
+              print("To ${(listArray[source] as PictureObj).picIndex}");
+
+              print("D ${(listArray[dest] as PictureObj).picIndex}");
+              (listArray[dest] as PictureObj).picIndex = source;
+              print("To ${(listArray[dest] as PictureObj).picIndex}");
+
+              listArray[source] = listArray[dest];
+              listArray[dest] = tempObj;
+              setState(() {
+                listArray = listArray;
+              });
+            });
+        listings.add(pic);
       }
 
       listings.add(new Stack(
@@ -237,7 +328,16 @@ class _EditProjectPageState extends State<EditProjectPage> {
             ),
           ),
           Expanded(
-              child: GridView.count(crossAxisCount: 2, children: listArray)),
+              child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                      maxCrossAxisExtent: 200,
+                      childAspectRatio: 3 / 2,
+                      crossAxisSpacing: 20,
+                      mainAxisSpacing: 20),
+                  itemCount: listArray.length,
+                  itemBuilder: (BuildContext ctx, index) {
+                    return listArray[index];
+                  })),
           TextButton(
               onPressed: () {
                 _getListings(arguments["folderPath"], arguments);
@@ -253,7 +353,78 @@ class _EditProjectPageState extends State<EditProjectPage> {
                   child: TextButton(
                 child: Text("Save"),
                 onPressed: () {
-                  debugPrint("Saving to project...");
+                  debugPrint("Saving to project ${listArray.length} files...");
+                  debugPrint(arguments["folderPath"] + "/config.json");
+                  File jsonFile =
+                      File(arguments["folderPath"] + "/config.json");
+
+                  Map<String, dynamic> jsonFileContent = {};
+                  if (jsonFile.existsSync()) {
+                    Map<String, dynamic> jsonFileContent =
+                        json.decode(jsonFile.readAsStringSync());
+                    print(jsonFileContent);
+                  }
+
+                  List<String> picture_order = [];
+                  for (var i = 0; i < listArray.length; i++) {
+                    if (i == listArray.length - 1) {
+                      continue;
+                    }
+                    print("jiz");
+                    var picObj = (listArray[i] as PictureObj);
+                    print(picObj.filePath);
+                    picture_order.add(picObj.filePath);
+                    //File file = File(picObj.filePath);
+                    //String newFileName = "Page ${picObj.picIndex}.jpg";
+                    //var path = file.path;
+                    //var lastSeparator =
+                    //    path.lastIndexOf(Platform.pathSeparator);
+                    //var newPath =
+                    //    path.substring(0, lastSeparator + 1) + newFileName;
+                    //print("From ${picObj.filePath} to $newPath");
+                    //file.rename(newPath);
+                    //picObj.filePath = newPath;
+                  }
+                  jsonFileContent["picture_order"] = picture_order;
+
+                  // for (var i = 0; i < listArray.length; i++) {
+                  //   if (i == listArray.length - 1) {
+                  //     continue;
+                  //   }
+                  //   print("jiz");
+                  //   var picObj = (listArray[i] as PictureObj);
+                  //   File file = File(picObj.filePath);
+                  //   String newFileName = "Page xxxxxx ${picObj.picIndex}.jpg";
+                  //   var path = file.path;
+                  //   var lastSeparator =
+                  //       path.lastIndexOf(Platform.pathSeparator);
+                  //   var newPath =
+                  //       path.substring(0, lastSeparator + 1) + newFileName;
+                  //   print("From ${picObj.filePath} to $newPath");
+                  //   file.rename(newPath);
+                  //   picObj.filePath = newPath;
+                  // }
+
+                  // for (var i = 0; i < listArray.length; i++) {
+                  //   if (i == listArray.length - 1) {
+                  //     continue;
+                  //   }
+                  //   print("jiz");
+                  //   var picObj = (listArray[i] as PictureObj);
+                  //   print(picObj.filePath);
+                  //   File file = File(picObj.filePath);
+                  //   String newFileName = "Page ${picObj.picIndex}.jpg";
+                  //   var path = file.path;
+                  //   var lastSeparator =
+                  //       path.lastIndexOf(Platform.pathSeparator);
+                  //   var newPath =
+                  //       path.substring(0, lastSeparator + 1) + newFileName;
+                  //   print("From ${picObj.filePath} to $newPath");
+                  //   file.rename(newPath);
+                  //   picObj.filePath = newPath;
+                  // }
+
+                  jsonFile.writeAsStringSync(json.encode(jsonFileContent));
                 },
                 style: TextButton.styleFrom(
                     primary: Colors.white,
