8b6045a018e8dad5b4f4a735aa08361a0f8d9d2b
Jiao Shaobo <shaobojiao@outlook.com>
2021-11-05 22:01:59 +0800

    trival

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:ee3080_dip049/DrawingScreen.dart';
 import 'package:flutter/material.dart';
 import 'HomeScreen.dart';
 import 'CameraPage.dart';
@@ -26,6 +27,10 @@ class MyApp extends StatelessWidget {
         '/edit_page': (context) => EditProjectPage(
             extraArgs: ModalRoute.of(context)!.settings.arguments),
         '/process': (context) => ProcessingScreen(),
+        '/draw': (context) => DrawingScreen(),
+        // '/export_page': (context) => ExportPage(),
+        '/export_page': (context) =>
+            ExportPage(ModalRoute.of(context)!.settings.arguments as String),
       },
     );
   }
